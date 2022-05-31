86afe3b01b0b42528c391ab3dbdce8a0a5d6177f
JuanPaLA <juanlopezalurralde@gmail.com>
2022-05-31 12:50:33 -0300

    minor changes - switching to other pc

--- routes/varsRoutes.js
+++ routes/varsRoutes.js
@@ -3,8 +3,14 @@ var varsRouter = express.Router();
 
 var vars = require('../models/vars')
 
-varsRouter.get('/', async(req, res)=>{ 
+varsRouter.get('/', async (req, res)=>{ 
     res.send( await vars.find({}) )
+})
+
+varsRouter.post('/', async (req, res)=>{
+    console.log(req.body);
+    const {name,type,description,timeframe,created,user,share,tags} = req.body.data;
+    console.log(name,type,description,timeframe,created,user,share,tags);
 })
 
 module.exports = varsRouter;

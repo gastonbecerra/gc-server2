b7ac0f45be18b9a92704dd2a96a89cc3140d1ac5
JuanPaLA <juanlopezalurralde@gmail.com>
2022-06-04 17:15:54 -0300

    minor changes

--- routes/varsRoutes.js
+++ routes/varsRoutes.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var varsRouter = express.Router();
 
-var vars = require('../models/vars')
+var Var = require('../models/vars')
 
 varsRouter.get('/', async (req, res)=>{ 
-    res.send( await vars.find({}) )
+    res.send( await Var.find({}) )
 })
 
-varsRouter.post('/', async (req, res)=>{
-    console.log(req.body);
-    const {name,type,description,timeframe,created,user,share,tags} = req.body.data;
-    console.log(name,type,description,timeframe,created,user,share,tags);
+varsRouter.post('/', async (req, res)=>{    
+    const {name,type,description,timeframe,created,user,tags} = req.body.data;
+    
+    const newVar = new Var({
+        name,
+        type,
+        description,
+        timeframe,
+        created,
+        user,
+        tags
+    })
+
+    newVar.save()
+    .then((data)=>{
+        res.send(data)
+    })
+    .catch((e)=>{
+        res.status(404).send(e);
+    })
+})
+
+varsRouter.put('/:id', async  (req, res, next)=>{
+  Var.findByIdAndUpdate(req.params.id, {
+    $set: req.body.data
+  }, (error, data) => {
+    if (error) {
+      return next(error);
+    } else {
+      res.json(true)
+    }
+  })
 })
 
 module.exports = varsRouter;
